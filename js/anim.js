3ece80b9a83a13dbf6188d67c8fe623f3dc0b18d
razik <ellifaz.marciano@outlook.com>
2019-06-25 10:34:05 -0300

    primeiro teste

---
+++ js/anim.js
@@ -0,0 +1,25 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    // anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8'],
+    // navigationTooltips: ['Início', 'João e Maria', 'Filisbino e Filisbina', 'Pedro e Paula', 'Lucia e Luciana', 'Zé e Joana', 'Sobre', 'Contato'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+    // dragAndMove: true,
+    responsiveWidth: 800,
+    scrollOverflow: true,
+    css3:false
+})
+
+new fullpage('#jobfullpage', {
+    autoScrolling: true,
+    navigation: true,
+    // anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8'],
+    // navigationTooltips: ['Início', 'João e Maria', 'Filisbino e Filisbina', 'Pedro e Paula', 'Lucia e Luciana', 'Zé e Joana', 'Sobre', 'Contato'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+    // dragAndMove: true,
+    responsiveWidth: 4000,
+    scrollOverflow: true,
+    css3:false
+})
