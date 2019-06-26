87ee4636abb2d8f53bdb29c47a2d61fa50e2e0fa
razik <ellifaz.marciano@outlook.com>
2019-06-26 19:41:08 -0300

    alteracao maiusculos

---
+++ footer.php
@@ -0,0 +1,24 @@
+        <footer class="section fp-auto-height footer-home">
+            <div class="footer-box">
+                <div class="footer-inicio">
+                    <a href="index.html"><span class="iconify icone" data-icon="mdi:home-outline" data-inline="false"></span><br><span class="descr">voltar ao início</span></a>
+                </div>
+                <div class="footer-copyright">
+                    <h1>2019 © My Life Fotografias</h1>
+                    <p>Criado e desenvolvido por <a href="#">Girapix</a></p>
+                </div>
+                <div class="footer-topo">
+                    <a href="index.html"><span class="iconify icone" data-icon="mdi:chevron-double-up" data-inline="false"></span><br><span class="descr">voltar ao topo</span></a>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/vendors/scrolloverflow.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/fullpage.min.js"></script>
+    <script src="js/anim.js"></script>
+    <script src="js/init.js"></script>
+</body>
+</html>
---
+++ includes/pages/home.php
@@ -0,0 +1,145 @@
+<div id="fullpage">
+    <section class="section portfolio e1">
+        <div class="portfolio-desc">
+            <h1>Minha vida eternizada através de imagens.</h1>
+            <p>"Amar não é olhar um para o outro, é olhar juntos na mesma direção."</p>
+            <h3 class="autor">Antoine de Saint-Exupéry</h3>
+            <a href="valdeir-telma.html" class="botao btn-principal">Ver ensaio</a>
+        </div>
+        <div class="portfolio-pag">
+            <p><span class="atual">01</span><span class="total">/06</span></p>
+        </div>
+    </section>
+    <section class="section portfolio e2">
+        <div class="portfolio-desc">
+            <h1>O amor é a força mais sutil do mundo.</h1>
+            <p>"Amar uma pessoa significa querer envelhecer com ela."</p>
+            <h3 class="autor">Albert Camus</h3>
+            <a href="#" class="botao btn-principal">Ver ensaio</a>
+        </div>
+        <div class="portfolio-pag">
+            <p><span class="atual">02</span><span class="total">/06</span></p>
+        </div>
+    </section>
+    <section class="section portfolio e3">
+        <div class="portfolio-desc">
+            <h1>Ser amado é passado; Amar é durar.</h1>
+            <p>"O coração tem razões que a própria razão desconhece."</p>
+            <h3 class="autor">Blaise Pascal</h3>
+            <a href="#" class="botao btn-principal">Ver ensaio</a>
+        </div>
+        <div class="portfolio-pag">
+            <p><span class="atual">03</span><span class="total">/06</span></p>
+        </div>
+    </section>
+    <section class="section portfolio e4">
+        <div class="portfolio-desc">
+            <h1>O coração põe vida em tudo aquilo que ama.</h1>
+            <p>"Amar é encontrar na felicidade de outrem a própria felicidade."</p>
+            <h3 class="autor">Gottfried Leibnitz</h3>
+            <a href="#" class="botao btn-principal">Ver ensaio</a>
+        </div>
+        <div class="portfolio-pag">
+            <p><span class="atual">04</span><span class="total">/06</span></p>
+        </div>
+    </section>
+    <section class="section portfolio e5">
+        <div class="portfolio-desc">
+            <h1>Não há jardins sem flores, nem coração sem amor.</h1>
+            <p>"Jurei te amar eternamente, mas como não sou eterno, juro te amar até o fim da minha vida."</p>
+            <h3 class="autor">Autor desconhecido</h3>
+            <a href="#" class="botao btn-principal">Ver ensaio</a>
+        </div>
+        <div class="portfolio-pag">
+            <p><span class="atual">05</span><span class="total">/06</span></p>
+        </div>
+    </section>
+    <section class="section portfolio e6">
+        <div class="portfolio-desc">
+            <h1>Aquilo que o coração ama fica eterno.</h1>
+            <p>"Quando se ama não é preciso entender o que se passa lá fora, pois tudo passa a acontecer dentro de nós."</p>
+            <h3 class="autor">Paulo Coelho</h3>
+            <a href="#" class="botao btn-principal">Ver ensaio</a>
+        </div>
+        <div class="portfolio-pag">
+            <p><span class="atual">06</span><span class="total">/06</span></p>
+        </div>
+    </section>
+    <section class="section sobre">
+        <div class="sobre-box">
+            <div class="sobre-chamada">
+                <h1>Mais que um time,<br> uma família</h1>
+                <a href="equipe.html" class="botao btn-secundario">veja mais</a>
+            </div>
+            <div class="sobre-texto">
+                <p>A My life é uma empresa que já possui alguns anos de experiência, na verdade 20 anos já nesta caminhada fotografica, duas décadas buscando a  erfeita fotografia respeitando sempre os sonhos e os  momentos de cada cliente, o que torna cada ensaio único.</p>
+                <p>Único porque cada pessoa, cada casal tem uma história tem um sentimento, e a nossa equipe tem buscado com bastante obstinação valorizar antes de tudo o sonho o romantismos porque cada momento é único.</p>
+            </div>
+        </div>
+    </section>
+    <section class="section contato fp-auto-height-responsive" id="contato">
+        <div class="row container titulo">
+            <h1>Deixe sua mensagem para a gente!</h1>
+        </div>
+        <div class="row container formulario">
+            <form action="" autocomplete="off">
+                <div class="input-field col s12 m6">
+                    <input type="text" id="nome" class="validate" data-lenght="60">
+                    <label for="nome">Me diz seu nome?</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="text" autocomplete="off" id="number" class="validate" data-lenght="30">
+                    <label for="number" data-error="wrong" data-success="right">E seu telefone, qual é?</label>
+                </div>
+                <div class="input-field col s12">
+                    <textarea id="textarea1" class="materialize-textarea" data-leght="250"></textarea>
+                    <label for="textarea1">Me fale sobre seu projeto...</label>
+                </div>
+                <button class="botao waves-effect waves-light right btn-terciario" type="submit" name="action">Enviar</button>
+            </form>
+        </div>
+        <div class="row container dados-contato">
+            <div class="col s12 l6 mapa">
+                <div class="col s12 l6" id="map">
+                    <iframe src="https://snazzymaps.com/embed/163450" width="100%" height="600px" style="border:none;"></iframe>
+                </div>
+            </div>
+            <div class="col s12 l6">
+                <div class="row">
+                    <div class="col s3 l4 offset-l1">
+                        <strong>Endereço:</strong>
+                    </div>
+                    <div class="col s9 l7">
+                        <p>Shopping das Noivas, Sala 02</p>
+                        <p>R. José Hermano, 763 - St. Campinas</p>
+                        <p>Goiânia - GO, 74515-030</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s3 l4 offset-l1">
+                        <strong>Telefone:</strong>
+                    </div>
+                    <div class="col s9 l7">
+                        <p>62 3941-6468</p>
+                        <p>62 9 8600-1209</p>
+                        <p>62 9 8516-2073</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s3 l4 offset-l1">
+                        <strong>Email:</strong>
+                    </div>
+                    <div class="col s9 l7">
+                        <p>mylifefotografias@gmail.com</p>
+                    </div>
+                </div>
+                <div class="row sociais">
+                    <div class="sociais-box">
+                        <a href="#"><span class="iconify" data-icon="mdi:facebook" data-inline="false"></span></a>
+                        <a href="#"><span class="iconify" data-icon="mdi:whatsapp" data-inline="false"></span></a>
+                        <a href="#"><span class="iconify" data-icon="mdi:instagram" data-inline="false"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
